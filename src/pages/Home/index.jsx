a22354e63718dc35d21956ba0bbd154b633dfad5
Laurent Janet <laurent.janet@gmail.com>
2020-04-20 15:34:08 +0200

    initializes Master page

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Home = () => (<div className="option-list">
+  <div className="option">
+    <p>Create a new session. You need a valid Spotify account.</p>
+    <a href="/create-session">Create &gt;&gt;</a>
+  </div>
+</div>)
+
+export { Home };
---
+++ src/pages/Master/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Master = () => (
+  <div>
+    <p>New session</p>
+  </div>
+);
+
+export { Master };
