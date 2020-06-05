39a3946026479f0c39e207018789adf1532d7c32
Laurent Janet <laurent.janet@gmail.com>
2020-06-05 19:55:32 +0200

    adds tests for Player.js component

---
+++ src/components/Player/Player.spec.js
@@ -0,0 +1,93 @@
+import React from "react";
+
+import { render, fireEvent } from "@testing-library/react";
+import { Player } from "./Player";
+
+import { SpotifyContext } from "../../contexts/Spotify";
+
+describe("<Player />", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  const state = {
+    track_window: {
+      current_track: { name: "currentTrack" },
+      next_tracks: [{ name: "nextTrack" }],
+    },
+    paused: true,
+  };
+
+  it("should display the current track name", () => {
+    const setPlayerStateChangeCb = (setState) => setState(state);
+    const getPlayer = jest.fn(() => ({
+      togglePlay: jest.fn(),
+      nextTrack: jest.fn(),
+    }));
+
+    const { getByText } = render(
+      <SpotifyContext.Provider value={{ setPlayerStateChangeCb, getPlayer }}>
+        <Player nextTrackCallback={() => {}} />
+      </SpotifyContext.Provider>
+    );
+
+    expect(getByText(/currentTrack/)).toBeTruthy();
+  });
+
+  it("should display the next track name", () => {
+    const setPlayerStateChangeCb = (setState) => setState(state);
+    const getPlayer = jest.fn(() => ({
+      togglePlay: jest.fn(),
+      nextTrack: jest.fn(),
+    }));
+
+    const { getByText } = render(
+      <SpotifyContext.Provider value={{ setPlayerStateChangeCb, getPlayer }}>
+        <Player nextTrackCallback={() => {}} />
+      </SpotifyContext.Provider>
+    );
+
+    expect(getByText(/nextTrack/)).toBeTruthy();
+  });
+
+  it("should call togglePlay on play/pause button click", () => {
+    const setPlayerStateChangeCb = (setState) => setState(state);
+    const mockTogglePlay = jest.fn();
+    const getPlayer = jest.fn(() => ({
+      togglePlay: mockTogglePlay,
+      nextTrack: jest.fn(),
+    }));
+
+    const { getByText } = render(
+      <SpotifyContext.Provider value={{ setPlayerStateChangeCb, getPlayer }}>
+        <Player nextTrackCallback={() => {}} />
+      </SpotifyContext.Provider>
+    );
+
+    fireEvent.click(getByText("Play"));
+
+    expect(mockTogglePlay).toHaveBeenCalled();
+  });
+
+  it("should call nextTrack and nextTrackCallback on 'Play next track' button click", () => {
+    const setPlayerStateChangeCb = (setState) => setState(state);
+    const mockTogglePlay = jest.fn();
+    const mockNextTrack = jest.fn();
+    const mockNextTrackCallback = jest.fn();
+    const getPlayer = jest.fn(() => ({
+      togglePlay: mockTogglePlay,
+      nextTrack: mockNextTrack,
+    }));
+
+    const { getByText } = render(
+      <SpotifyContext.Provider value={{ setPlayerStateChangeCb, getPlayer }}>
+        <Player nextTrackCallback={mockNextTrackCallback} />
+      </SpotifyContext.Provider>
+    );
+
+    fireEvent.click(getByText("Play next track"));
+
+    expect(mockNextTrack).toHaveBeenCalled();
+    expect(mockNextTrackCallback).toHaveBeenCalled();
+  });
+});
